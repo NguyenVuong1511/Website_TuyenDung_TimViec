700de63190eb7e984928fc6af096110f28083f34
NguyenVuong1511 <nguyenvuong15112005@gmail.com>
2026-03-27 23:16:10 +0700

    Them Ocelot

---
+++ BE/ApiGateway/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. Yêu cầu hệ thống nạp cấu hình từ file ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// 2. Đăng ký dịch vụ Ocelot
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello from API Gateway!");
+
+// 3. Sử dụng Middleware của Ocelot (Phải dùng await)
+await app.UseOcelot();
+
+app.Run();
